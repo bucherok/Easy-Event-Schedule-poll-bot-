b50b014848d46a36f9cc7775f7d39ceeeb3aff21
bucherok <bucherok@ya.ru>
2017-10-09 20:06:00 +0300

    added logic

--- index.js
+++ index.js
@@ -1,15 +1,104 @@
-const TelegramBot = require('node-telegram-bot-api')
-const TG_TOKEN = '269653246:AAFp9-NzZUc03C6VPGjlZOp_PlttWvUEzRo'
+const TelegramBot = require('node-telegram-bot-api');
+const TG_TOKEN = '269653246:AAFp9-NzZUc03C6VPGjlZOp_PlttWvUEzRo';
 
-let tgBot = new TelegramBot(TG_TOKEN, { polling: true })
+let tgBot = new TelegramBot(TG_TOKEN, { polling: true });
 
-tgBot.on('text', msg => {
-    traceMsg(msg)
-    tgBot.sendMessage(msg.chat.id, `ага`)
-})
+
+var event = {};
+
+
+tgBot.on('text', (msg) => {
+
+    traceMsg(msg);
+
+    if (msg.text.match(/\/new/)) {
+        addNewEvent(msg)
+    }
+
+    else if (event.waitForLabel) {
+        addNewEvent(msg)
+    }
+
+    else if (event.waitForPossibleDays) {
+        if (msg.text.match(/\/end/)) {
+            event.waitForPossibleDays = false
+            return
+        }
+        addPossibleEventDay(msg)
+    }
+
+    else if (msg.text.match(/\/eventStats/)) {
+        getEventResults(msg)
+
+    }
+
+});
+
+function addNewEvent(msg) {
+
+
+    if (event.waitForLabel) {
+        event.label = msg.text
+        event.waitForLabel = false
+        let reply = `Okay, lets add possible days for '${event.label}'.\nSend options.`
+        tgBot.sendMessage(msg.chat.id, reply)
+            .then(function () {
+
+                event.waitForPossibleDays = true
+                event.possibleDays = []
+
+             })
+    }
+    else {
+        let reply = 'Hi!\nPlease enter label of new event you would like to plan';
+        tgBot.sendMessage(msg.chat.id, reply)
+            .then(function () {
+
+                event.waitForLabel = true
+
+             })
+    }
+
+}
+
+function addPossibleEventDay(msg) {
+
+    event.possibleDays.push({
+        name: msg.text,
+        id: msg.message_id,
+        participants: 1
+    })
+
+    let reply = `Okay, you added next days for '${event.label}':\n`
+
+    event.possibleDays.forEach((item) => {
+        reply += `🎈${item.name}\n`
+    })
+
+    reply += 'Send more options or \'/end\' to complete the list';
+    let keyboard = {
+
+    }
+    tgBot.sendMessage(msg.chat.id, reply, keyboard)
+}
+
+function getEventResults(msg) {
+    let reply = `So, for this moment on ⚠️${event.label}⚠️ band are ready at\n`
+
+    event.possibleDays.forEach((item) => {
+        reply += `🎈${item.name} - ${item.participants} people(s)\n`
+    })
+
+    tgBot.sendMessage(msg.chat.id, reply)
+}
+
+function addMyDay() {
+
+}
+
 
 
 function traceMsg(msg) {
-    console.log(msg)
+    console.log(msg);
     console.log('\n =====-----===== \n')
 }
